7ddf6afc8abfd588a53d16437b7e690188592bed
Fabian Buijing <fabianbuijing@gmail.com>
2018-04-30 14:42:58 +0200

    smaller illustrations
    added GSAP animation
    cleaned CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-// no JS yet
+
+const tl = new TimelineMax({
+    repeat: -1
+})
+
+tl.fromTo(".dashline", 10, {
+    strokeDashoffset: (170)
+}, {
+    strokeDashoffset: 0,
+    ease:Linear.easeNone
+})
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY
+    const illustrationsHeight = document.querySelector('.landing_illustrations').getBoundingClientRect().height;
+
+    if (scrollY > illustrationsHeight) {
+        tl.pause()        
+    } else {
+        tl.play()
+    }
+})
